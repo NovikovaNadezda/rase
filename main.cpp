aaf9a91616deb411a81d68b690e90eeae5ad1554
NovikovaNadezda <114093562+NovikovaNadezda@users.noreply.github.com>
2023-02-22 18:31:56 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,66 @@
+﻿#include <SFML/Graphics.hpp>
+#include "settings.h"
+#include "Roadobj.h"
+#include "player.h"
+#include "obstacle.h"
+#include "text.h"
+using namespace sf;
+
+int main()
+{
+
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE, Style::Close | Style::Titlebar);
+	window.setPosition(Vector2i{ (1920 - (int)WINDOW_WIDTH) / 2,0 });
+	window.setFramerateLimit(FPS);
+
+	Roadobj road1, road2;
+	roadObjInit(road1, ROAD1_START_POS, Doroga_File_Name, 100.f);
+	roadObjInit(road2, ROAD2_START_POS, Doroga_File_Name, 100.f);
+	Roadobj grass1, grass2;
+	roadObjInit(grass1, GRASS1_START_POS, Trava_File_Name, 0.f);
+	roadObjInit(grass2, GRASS2_START_POS, Trava_File_Name, 0.f);
+	Player player;
+	playerInit(player);
+	Obstacle obs1;
+	obstacleInit(obs1, "car.png");
+	TextObj scoreText;
+	textInit(scoreText, std::to_string(score), TEXT_POS);
+
+
+	while (window.isOpen())
+	{
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+
+			if (event.type == Event::Closed)
+
+				window.close();
+		}
+		//Update
+		roadObjUpdate(grass1);
+		roadObjUpdate(grass2);
+		roadObjUpdate(road1);
+		roadObjUpdate(road2);
+		playerUpdate(player);
+		obstacleUpdate(obs1);
+		FloatRect playerHitBox = player.sprite.getGlobalBounds();
+		FloatRect obs1HitBox = obs1.sprite.getGlobalBounds();
+		if (playerHitBox.intersects(obs1HitBox)) {
+			
+		}
+		textUpdate(scoreText, score);
+		//Draw
+		roadObjDraw(window, grass1);
+		roadObjDraw(window, grass2);
+		//RoadObjDraw(window, Pesok);
+		roadObjDraw(window, road1);
+		roadObjDraw(window, road2);
+		playerDraw(window, player);
+		obstacleDraw(window, obs1);
+		window.display();
+	}
+
+	return 0;
+}
---
+++ settings.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+
+const float WINDOW_WIDTH = 800;
+const float WINDOW_HEIGHT = 1000;
+const std::string WINDOW_TITLE = "SFML Racing P53122";
+const float FPS = 60.f;
+
+const sf::Vector2f GRASS1_START_POS{ 0.f, 0.f };
+const sf::Vector2f GRASS2_START_POS{ 0.f, -WINDOW_HEIGHT };
+const std::string Trava_File_Name("grass.jpg");
+
+const sf::Vector2f ROAD1_START_POS{ 100.f, 0.f };
+const sf::Vector2f ROAD2_START_POS{ 100.f, -WINDOW_HEIGHT };
+const std::string Doroga_File_Name("road.png");
+
+const float ROADOBJ_SPEEDY = 10.f;
+const float LEFT_ROADSIDE = 100.f;
+const float RIGHT_ROADSIDE = 500.f;
+
+const std::string PLAYER_FILE_NAME("car.png");
+const float PLAYER_WIDTH = 85.f;
+const float PLAYER_HEIGHT = 168.f;
+sf::Vector2f PLAYER_START_POS{
+	(WINDOW_WIDTH - PLAYER_WIDTH) / 2.f,
+	WINDOW_HEIGHT - PLAYER_HEIGHT - 10.f
+};
+const float PLAYER_SPEED = 5.f;
+const sf::Vector2f OBCTACLE_START_POS{ 0.f, 0.f };
+const float OBCTACLE_SPEEDY = 5.f;
+const sf::Vector2f obstacle{ 100.f, 0.f };
+const std::string obstacle_File_Name("car(1).png");
+const unsigned int FONT_SIZE = 64;
+const sf::Vector2f TEXT_POS{ 750.f, -900.f };
